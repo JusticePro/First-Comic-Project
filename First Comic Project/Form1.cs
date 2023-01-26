5f1bbb7a0e211d77ba02d007a3ee9e4a7cb3d611
Justice Ben-Ezra <justicedbenezra@gmail.com>
2023-01-26 15:24:15 -0600

    Move operation controls to OperationManager

--- First Comic Project/Form1.cs
+++ First Comic Project/Form1.cs
@@ -20,6 +20,7 @@ namespace First_Comic_Project
     {
         OpenFileDialog open = new OpenFileDialog();
         public static Form1 instance;
+        private OperationManager operationManager;
 
         public UserControl episodeSelection;
 
@@ -27,6 +28,9 @@ namespace First_Comic_Project
         {
             InitializeComponent();
             instance = this;
+
+            // Setup Operation Manager
+            this.operationManager = new OperationManager(this);
         }
 
         string getExportPath()
@@ -54,96 +58,26 @@ namespace First_Comic_Project
             });
         }
 
-        #region Process Episodes
+        /// <summary>
+        /// Start any type of operation.
+        /// </summary>
         void startOperation()
         {
             IEnumerable<int> episodes = ((EpisodeSelection)episodeSelection).getEpisodes();
 
+            // Episodes may be null if there was an issue in its selection.
             if (episodes != null)
             {
+                // Make graphical changes.
                 Cursor = Cursors.WaitCursor;
                 buttonStart.Enabled = false;
-
                 progressBar.Maximum = episodes.Count();
 
-                Thread thread = new Thread(new ThreadStart(() => processBulk(episodes)));
-                thread.Start();
-            }
-        }
-
-        void processBulk(IEnumerable<int> episodes)
-        {
-
-            // For each episode
-            foreach (int i in episodes)
-            {
-                try
-                {
-                    setLabel("Gathering Episode #" + i);
-
-                    Image episode = Gatherer.getComic(i);
-
-                    if (episode == null)
-                    {
-                        continue;
-                    }
-
-                    episode.Save(Path.Combine(getExportPath(), "Episode #" + i + ".png"));
-
-                    if (checkBoxSeparate.Checked)
-                    {
-                        setLabel("Separating the panels for Episode #" + i);
-
-                        string episodeDirectory = Path.Combine(getExportPath(), "Episode #" + i);
-
-                        if (!Directory.Exists(episodeDirectory))
-                        {
-                            Directory.CreateDirectory(episodeDirectory);
-                        }
-
-                        //Separator.separateAndExportPanels(episode, colorDialog1.Color, Path.Combine(episodeDirectory, "Panel #{id}.png"));
-
-                        List<Image> images = Separator.separatePanels(episode, colorDialog1.Color);
-
-                        int x = 0;
-                        foreach (Image panel in images)
-                        {
-                            panel.Save(Path.Combine(episodeDirectory, "Panel #" + x + ".png"));
-                            panel.Dispose();
-                            x++;
-                        }
-                        episode.Dispose();
-                    }
-
-                    BeginInvoke((MethodInvoker)delegate ()
-                    {
-                        progressBar.Value++;
-                    });
-                }catch (Exception e)
-                {
-                    string episodeDirectory = Path.Combine(getExportPath(), "Episode #" + i);
-
-                    if (!Directory.Exists(episodeDirectory))
-                    {
-                        Directory.CreateDirectory(episodeDirectory);
-                    }
-                    Debug.WriteLine("Failed episode " + i + ": " + e.StackTrace);
-                }
+                // Start operation.
+                operationManager.startOperation(episodes);
             }
 
-            BeginInvoke((MethodInvoker)delegate ()
-            {
-                progressBar.Value = 0;
-
-                buttonStart.Enabled = true;
-                Cursor = Cursors.Default;
-            });
-            setLabel("");
-
-            SystemSounds.Beep.Play();
-            Process.Start(getExportPath());
         }
-        #endregion
 
         void setEpisodeControl(UserControl control)
         {
---
+++ First Comic Project/OperationManager.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace First_Comic_Project
+{
+    class OperationManager
+    {
+        private Form1 form;
+        public OperationManager(Form1 form)
+        {
+            this.form = form;
+        }
+
+        public void startOperation(IEnumerable<int> episodes)
+        {
+            Thread thread = new Thread(new ThreadStart(() => processBulk(episodes)));
+            thread.Start();
+        }
+
+        void processBulk(IEnumerable<int> episodes)
+        {
+
+            // For each episode
+            foreach (int i in episodes)
+            {
+                try
+                {
+                    setLabel("Gathering Episode #" + i);
+
+                    Image episode = Gatherer.getComic(i);
+
+                    if (episode == null)
+                    {
+                        continue;
+                    }
+
+                    episode.Save(Path.Combine(getExportPath(), "Episode #" + i + ".png"));
+
+                    if (checkBoxSeparate.Checked)
+                    {
+                        setLabel("Separating the panels for Episode #" + i);
+
+                        string episodeDirectory = Path.Combine(getExportPath(), "Episode #" + i);
+
+                        if (!Directory.Exists(episodeDirectory))
+                        {
+                            Directory.CreateDirectory(episodeDirectory);
+                        }
+
+                        //Separator.separateAndExportPanels(episode, colorDialog1.Color, Path.Combine(episodeDirectory, "Panel #{id}.png"));
+
+                        List<Image> images = Separator.separatePanels(episode, colorDialog1.Color);
+
+                        int x = 0;
+                        foreach (Image panel in images)
+                        {
+                            panel.Save(Path.Combine(episodeDirectory, "Panel #" + x + ".png"));
+                            panel.Dispose();
+                            x++;
+                        }
+                        episode.Dispose();
+                    }
+
+                    BeginInvoke((MethodInvoker)delegate ()
+                    {
+                        progressBar.Value++;
+                    });
+                }
+                catch (Exception e)
+                {
+                    string episodeDirectory = Path.Combine(getExportPath(), "Episode #" + i);
+
+                    if (!Directory.Exists(episodeDirectory))
+                    {
+                        Directory.CreateDirectory(episodeDirectory);
+                    }
+                    Debug.WriteLine("Failed episode " + i + ": " + e.StackTrace);
+                }
+            }
+
+            BeginInvoke((MethodInvoker)delegate ()
+            {
+                progressBar.Value = 0;
+
+                buttonStart.Enabled = true;
+                Cursor = Cursors.Default;
+            });
+            setLabel("");
+
+            SystemSounds.Beep.Play();
+            Process.Start(getExportPath());
+        }
+    }
+}
