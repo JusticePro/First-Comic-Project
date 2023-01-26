32d68d51689c9af9ca0b62655233a9b7ea736adb
Justice Ben-Ezra <justicedbenezra@gmail.com>
2023-01-26 15:46:14 -0600

    Implement SeperatorSelection into the Seperator module.

--- First Comic Project/Operations/Selectors/SeparatorSelection.cs
+++ First Comic Project/Operations/Selectors/SeparatorSelection.cs
@@ -11,4 +11,32 @@ namespace First_Comic_Project.Operations.Selectors
     {
         bool isWhitespace(Color color);
     }
+
+    public class SeperatorUtils
+    {
+        static bool isEqual(Color color1, Color color2)
+        {
+            if (color1.A != color2.A)
+            {
+                return false;
+            }
+
+            if (color1.R != color2.R)
+            {
+                return false;
+            }
+
+            if (color1.G != color2.G)
+            {
+                return false;
+            }
+
+            if (color1.B != color2.B)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
 }
